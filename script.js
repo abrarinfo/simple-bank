0ad0d57ae5545ea47c5e5d07b18694627cae48ff
Abrar903 <princeofosun0003@gmail.com>
2021-01-29 23:09:50 +0600

    facing an problem

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
+// login button event handler
+
 const loginBtn = document.getElementById("loginBtn");
 loginBtn.addEventListener("click", function() {
     const loginArea = document.getElementById("login-section");
     loginArea.style.display = "none";
     const transactionArea = document.getElementById("transaction-area");
     transactionArea.style.display = "block";
+})
+
+// diposit/widthraw button event handler
+
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener('click', function () {
+
+    const depositAmount = document.getElementById("depositAmount").value;
+    const depositNumber = parseFloat(depositAmount)
+
+    const currentDeposit = document.getElementById('currentDeposit').innerText;
+    const currentDepositNumber = parseFloat(currentDeposit);
+    const totalDeposit = depositNumber + currentDepositNumber;
+    
+    document.getElementById("currentDeposit").innerText = totalDeposit;
+
+    const currentBalance = document.getElementById("currentBalance");
+    const currentBalanceNumber = parseFloat(currentBalance);
+    const totalBalance = depositNumber + currentBalanceNumber;
+    document.getElementById("currentDeposit").innerText = totalBalance;
+
+    document.getElementById("depositAmount").value = "";
 })
